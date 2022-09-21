91941c767d9e4f8481c0783f25c955332e17b01f
Mariano Paporello <mariano.paporello@gmail.com>
2022-09-21 14:40:26 -0300

    first commit

---
+++ primeraEntrega.js
@@ -0,0 +1,18 @@
+class Usuario{
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido
+        this.libros = libros;
+        this.mascotas=mascotas;
+    }
+    getFullName(){
+        console.log(`${this.nombre} ${this.apellido}`)
+    }
+    addMascota(input){
+        this.mascotas={...this.mascotas, input}
+    }
+    countMascotas(){
+        console.log(this.mascotas.lenght)
+    }
+    
+}
