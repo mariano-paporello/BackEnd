a33f04d43966245316bdf19ddc466072b725704c
Mariano Paporello <mariano.paporello@gmail.com>
2022-09-21 16:15:03 -0300

    primeraEntrega

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -6,18 +6,38 @@ class Usuario{
         this.mascotas=mascotas;
     }
     getFullName(){
-        console.log(`${this.nombre} ${this.apellido}`)
+        console.log(`El nombre del Usuario es: ${this.nombre} ${this.apellido}`);
     }
     addMascota(input){
-       this.mascotas.push(input)
+       this.mascotas.push(input);
+       return this.mascotas;
     }
     countMascotas(){
-        console.log(this.mascotas.lenght)
+        console.log(`La cantidad de mascotas del Usuario es: ${this.mascotas.length}`);
     }
     addBook(nameOfBook, Author){
-        this.libros = [...this.mascotas, {nombre:nameOfBook, autor:Author} ]
+        this.libros = [...this.libros, {nombre:nameOfBook, autor:Author} ];
+        return this.libros;
     }
     getBookNames(){
-        console.log(...this.libros.nombre)
+        let bookListNames = []
+        this.libros.forEach(object => {
+            bookListNames = [...bookListNames, object.nombre]
+            return bookListNames
+        });
+        console.log(` Los Nombres de los Libros del Usuario son: ${bookListNames.join(", ")}.`)
     }
 }
+const usuarioRandom = new Usuario("Carlos", "Perez", [{nombre:"secretos de la vida", autor:"lorem"}, {nombre:"libro Random", autor:"autor Random"}, {nombre:"OverLord", autor:"Kugane Maruyama"}], ["gato", "loro", "tortuga", "orangutan"])
+usuarioRandom.getFullName()
+usuarioRandom.addMascota("perro")
+usuarioRandom.countMascotas()
+usuarioRandom.addBook("El señor de los anillos", "J. R. R. Tolkien")
+usuarioRandom.getBookNames()
+
+
+
+
+
+
+
