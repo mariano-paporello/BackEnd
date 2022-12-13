06b40de90641da40415581fad215b78326faa551
Mariano Paporello <mariano.paporello@gmail.com>
2022-12-13 13:44:27 -0300

    11-OnceavaEntrega

--- 11-OnceavaEntrega/src/routes/index.ts
+++ 11-OnceavaEntrega/src/routes/index.ts
@@ -4,19 +4,7 @@ import { crear5Productos } from "../Controllers/testController";
 
 const rutaPrincipal: Router = Router();
 
-rutaPrincipal.get('/login', (req, res) => {
-    res.render("Login")
-})
-rutaPrincipal.get("/logout", (req, res)=>{
-    res.render("Logout")
-})
-rutaPrincipal.post('/login', (req, res) => {
-    console.log(req.params, req.body, req.query)
-    // const {userName} = req.body
-    // console.log(userName)
-    // res.cookie("userName", userName)
-    res.redirect("/")
-})
+
 rutaPrincipal.get("/normalize", async(req, res)=>{
     res.json(await getAllNorm())
 })
--- 11-OnceavaEntrega/src/services/server.ts
+++ 11-OnceavaEntrega/src/services/server.ts
@@ -1,21 +1,44 @@
 import express from 'express'
 import http from 'http'
-import morgan from 'morgan'
 import { engine } from 'express-handlebars'
 import rutaTest from "../routes/index"
 import path from 'path'
 import ProductoModel from '../models/products'
 import menssagesModel from '../models/messages'
-import CookieParser from "cookie-parser"
-import session from "express-session"
+import cookieParser from "cookie-parser"
+import session from 'express-session';
 
+declare module 'express-session' {
+    interface SessionData {
+      nombre: String;
+      contador: Number;
+    }
+  }
+  
 
 
 const app = express()
-app.use(morgan('dev'))
+app.use("/api",rutaTest)
+// Session Part:
+let logged = { islogged: false, isTimedOut: false, isDestroyed: false, nombre: '' }
+const unSegundo = 1000;
+const unMinuto = unSegundo * 60;
+const unaHora = unMinuto * 60;
+const unDia = unaHora * 24;
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'SuperSecreto',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: { maxAge: unMinuto }
+  })
+);
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
-app.use(CookieParser())
 
 
 
@@ -36,24 +59,53 @@ app.engine('hbs', engine({
 
 
 app.get('/', async (req, res) => {
-    if(req.cookies.userName){
+    if(req.session.nombre!=""&&req.session.nombre&&logged.islogged){
+        console.log(req.cookies.userName)
         ProductoModel.find({}).then(productos => {
             menssagesModel.find({}).then(mensajes => {
                 res.render('main', {
                     productos: productos.map(productoIndv => productoIndv.toJSON()),
                     mensajes: mensajes.map(mensajeIndv => mensajeIndv.toJSON()),
-                    user: req.cookies.userName
+                    user: req.session.nombre
                 })
             })
         })
     }
     else{
-        res.redirect("/api/login")
+        res.redirect("/login")
+    }
+})
+app.get('/login', (req, res) => {
+    res.render("Login")
+})
+app.get("/logout", (req, res)=>{
+    if(req.session){
+    res.render("Logout", {
+        user: req.session.nombre
+    })
+    req.session.destroy;
+    console.log(req.session)
+    }
+    else{
+        res.redirect("/")
+    }
+})
+app.post('/login', (req, res) => {
+    const {userName} = req.body
+    if(userName|| userName== ""){
+        req.session.nombre =  userName
+    logged.nombre= userName
+    logged.islogged= true
+    console.log(req.session)
+    res.redirect("/")
+    }
+    else{
+        throw new Error('Completar ')
     }
     
 })
 
-app.use("/api",rutaTest)
+
 
 const HTTPServer = new http.Server(app);
 
